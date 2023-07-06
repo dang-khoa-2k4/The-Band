35a8b9b45cec666020785c7f5841cd04c3ffa0fb
dang-khoa-2k4 <khoa.nguyentrandang@hcmut.edu.vn>
2023-07-06 17:23:10 +0700

    test-menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,24 @@
 const Btn_list = document.querySelectorAll('.js-buy-btn');
+const check_click = document.querySelectorAll('#nav li a[href*="#"]');
 const check_modal = document.querySelector('.js-modal');
 const check_close = document.querySelector('.js-close-btn');
 const check_container = document.querySelector('.js-container');
+var header = document.getElementById('header');
+var mobile_menu = document.getElementById('mobile-menu-btn');
 
+const header_Height = header.clientHeight;
+console.log(check_click);
+
+mobile_menu.onclick = function () {
+
+    var isOpen = header.clientHeight === header_Height;
+    if (isOpen) {
+        header.style.height = 'auto';
+    }
+    else {
+        header.style.height = null;
+    }
+}
 function show_modal() {
     check_modal.classList.add('open');
 }
@@ -16,6 +32,14 @@ function stop_event(event) {
 }
 for (const btn of Btn_list) {
     btn.addEventListener('click', show_modal);
+}
+
+for (var i = 0; i < check_click.length; i++) {
+    var menuItem = check_click[i];
+    
+    menuItem.onclick = function () {
+        header.style.height = null;
+    }
 }
 
 check_modal.addEventListener('click', hide_modal);
